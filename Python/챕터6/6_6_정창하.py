bd6321f121606b6ab20bf270ed9df2e593c693c6
changhaaa <59922665+changhaaa@users.noreply.github.com>
2022-05-15 22:53:43 +0900

    Update 6_6_정창하.py

--- Python/챕터6/6_6_정창하.py
+++ Python/챕터6/6_6_정창하.py
@@ -6,29 +6,31 @@ Created on Sun May 15 13:50:41 2022
 """
 
 def parenBalance(s):
-    stack = ListStack
-    cnt1 = 0
-    cnt2 = 0
-    cnt3 = 0
-    cnt4 = 0
-    cnt5 = 0
-    cnt6 = 0
+    stack1 = ListStack
+    stack2 = ListStack
+    stack3 = ListStack
     
     for element in s:
-        stack.push(element)
-        
-    for i in range(len(s)):
-        if stack.pop() == '(':
-            cnt1 += 1
-        elif stack.pop() == ')':
-            cnt2 += 1
-        elif stack.pop() == '[':
-            cnt3 += 1
-        elif stack.pop() == ']':
-            cnt4 += 1
-        elif stack.pop() == '{':
-            cnt5 += 1
-        elif stack.pop() == '}':
-            cnt6 += 1
-    
-    return (cnt1 == cnt2) & (cnt3 == cnt4) & (cnt5 == cnt6)
+        if element == '(':
+            stack1.push('(')
+        elif element == '[':
+            stack2.push('[')
+        elif element == '{':
+            stack3.push('{')
+        elif element == ')':
+            if stack1.isEmpty():
+                return False
+            else:
+                stack1.pop()
+        elif element == ']':
+            if stack2.isEmpty():
+                return False
+            else:
+                stack2.pop()
+        elif element == '}':
+            if stack3.isEmpty():
+                return False
+            else:
+                stack3.pop()
+                
+    return (stack1.isEmpty() & stack2.isEmpty() & stack3.isEmpty())
