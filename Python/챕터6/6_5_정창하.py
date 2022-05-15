b9bbd1f5d42e04f25b71952288adcfa91ad0c2a2
changhaaa <59922665+changhaaa@users.noreply.github.com>
2022-05-15 22:40:02 +0900

    Update 6_5_정창하.py

--- Python/챕터6/6_5_정창하.py
+++ Python/챕터6/6_5_정창하.py
@@ -7,16 +7,14 @@ Created on Sun May 15 12:33:03 2022
 
 def parenBalance(s):
     stack = ListStack
-    cnt1 = 0
-    cnt2 = 0
     
     for element in s:
-        stack.push(element)
-        
-    for i in range(len(s)):
-        if stack.pop() == '(':
-            cnt1 += 1
-        elif stack.pop() == ')':
-            cnt2 += 1
-    
-    return (cnt1 == cnt2)
+        if element == '(':
+            stack.push('(')
+        elif element == ')':
+            if stack.isEmpty():
+                return False
+            else:
+                stack.pop()
+                
+    return stack.isEmpty()
