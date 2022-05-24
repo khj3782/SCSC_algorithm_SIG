eb1db19f8caf292482d90ec74a8223bb7db507cd
김현진 <khj3782@naver.com>
2022-05-24 16:47:32 +0900

    Create 카드 구매하기_김현진.cpp

---
+++ C++/5주차/카드 구매하기_김현진.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+	int N;
+	cin >> N;
+
+	int P[N+1] = {0};
+	for(int i = 1; i < N+1; i++) cin >> P[i];
+
+	int res[N+1] = {0};
+	for(int i = 1; i < N+1; i++){
+		for(int j = 1; j < i+1; j++){
+			res[i] = max(res[i], res[i-j]+P[j]);
+		}
+	}
+
+	cout << res[N] << '\n';
+
+	return 0;
+}
